522461f0e6eb3778ab12820ee185c1e3f6f8f174
Saya Zheniskyzy <sayazheniskyzy2005@icloud.com>
2023-02-01 00:02:47 +0600

    lab02

---
+++ week1/lab02/ifelse/5.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+c = input()
+d = input()
+if a == b and c == d :
+  print("Hello")
---
+++ week1/lab02/ifelse/6.py
@@ -0,0 +1,6 @@
+a = input()
+b = input()
+c = input()
+d = input()
+if a == b or c == d:
+  print("Hello")
